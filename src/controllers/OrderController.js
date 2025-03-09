6714c1ccbeb215f8a9e7a0ca4c84c34d55a69fb9
deepakkalra483 <163745586+deepakkalra483@users.noreply.github.com>
2025-03-09 23:43:48 +0530

    controlleruppdate

--- src/controllers/OrderController.js
+++ src/controllers/OrderController.js
@@ -2,21 +2,32 @@ const admin = require("../config/firebase");
 
 const placeOrder = async (req, res) => {
   const { tokens, orderDetails } = req.body;
-  console.log("tokens--", orderDetails);
+  // console.log("tokens--", orderDetails);
   if (!tokens || tokens?.length == 0) {
     return res
       .status(400)
       .json({ success: false, message: "All fields are required." });
   }
   try {
-    const notificationPayload = {
-      notification: {
-        title: "New Order Placed",
-        body: `Order # has been placed for Table #.`,
+    const message = {
+      to: "deM-lu3nQcuKOuntT7tX8F:APA91bHHt1nDYByQL77WyERceyNBfixNRdOxzZrISOhN-UlsVpF4ekO003400QxvZu6ll3DUs0Hyshpje5yNw5dhUb8XSyIbpvPkbwA4HO3ii3wop2T08gs",
+      priority: "high",
+      content_available: true,
+      data: {
+        title: "Wake Up!",
+        body: "Open the full-screen activity",
+        force_open: true,
       },
+    };
+    const notificationPayload = {
+      // notification: {
+      //   title: "New Order Placed",
+      //   body: `Order # has been placed for Table #.`,
+      // },
       data: orderDetails,
       tokens: tokens,
     };
+
     const response = await admin
       .messaging()
       .sendEachForMulticast(notificationPayload);
